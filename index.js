7822e9772074b14e6010ff2b942bf5ebe6698c05
Yair Manor (Kuszpet) <yairk@cyb3riot.com>
2017-03-28 11:04:22 +0300

    HTML sanitization now done at the common JS layer

--- index.js
+++ index.js
@@ -2,4 +2,34 @@
  * Created by Yaron Muzikant on 29-Jan-17.
  */
 import { NativeModules } from 'react-native';
-module.exports = NativeModules.GatewayFinder;
+
+var NativeGatewayFinder = NativeModules.GatewayFinder;
+
+function htmlSanitize(str) {
+    if (!str) return str;
+
+    return str
+        .replace(/&/g, '')
+        .replace(/"/g, '')
+        .replace(/'/g, '')
+        .replace(/</g, '')
+        .replace(/>/g, '');
+}
+
+GatewayFinder =
+    {
+        getGatewayInfo: function (onSuccess, onError) {
+            NativeGatewayFinder.getGatewayInfo(
+                (gatewayInfo) => {
+                    onSuccess({
+                        ip: gatewayInfo.ip,
+                        ssid: htmlSanitize(gatewayInfo.ssid),
+                        bssid: htmlSanitize(gatewayInfo.bssid)});
+                },
+                onError
+            );
+        }
+    };
+
+module.exports = GatewayFinder;
+
--- ios/GatewayFinder.m
+++ ios/GatewayFinder.m
@@ -26,26 +26,10 @@ RCT_EXPORT_METHOD(getGatewayInfo:(RCTResponseSenderBlock)onGatewayInfoFetched on
     NSMutableDictionary *gatewayInfo = [NSMutableDictionary dictionary];
     
     [gatewayInfo setObject:routerIp forKey:@"ip"];
-    [gatewayInfo setObject:sanitizeString(bssid) forKey:@"bssid"];
-    [gatewayInfo setObject:sanitizeString(ssid) forKey:@"ssid"];
+    [gatewayInfo setObject:bssid forKey:@"bssid"];
+    [gatewayInfo setObject:ssid forKey:@"ssid"];
   
     onGatewayInfoFetched(@[gatewayInfo]);
-}
-
-NSString *sanitizeString(NSString *s)
-{
-    if (s == nil) return nil;
-    
-    NSString *tmp;
-    
-    tmp = [s stringByReplacingOccurrencesOfString:@"&" withString:@""];
-    tmp = [tmp stringByReplacingOccurrencesOfString:@"\"" withString:@""];
-    tmp = [tmp stringByReplacingOccurrencesOfString:@"<" withString:@""];
-    tmp = [tmp stringByReplacingOccurrencesOfString:@">" withString:@""];
-    tmp = [tmp stringByReplacingOccurrencesOfString:@"'" withString:@""];
-    tmp = [tmp stringByReplacingOccurrencesOfString:@"/" withString:@""];
-    
-    return tmp;
 }
 
 @end
